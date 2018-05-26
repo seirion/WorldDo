18e9577850ce87867953ecee128bb8d00607706f
Jeong-Min Lee <falsetru@gmail.com>
2018-05-26 12:19:03 +0900

    moshi for json parsing.

---
+++ app/src/main/java/com/seirion/worlddodook/data/NaverRealTimeData.kt
@@ -0,0 +1,37 @@
+package com.seirion.worlddodook.data
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class NaverRealTimeResponse(
+        val resultCode: String,
+        val result: NaverRealTimeResponseResult
+) {
+    companion object
+}
+
+@JsonClass(generateAdapter = true)
+data class NaverRealTimeResponseResult(
+        val time: String,
+        val areas: List<NaverRealTimeResponseArea>,
+        val pollingInterval: Int
+)
+
+@JsonClass(generateAdapter = true)
+data class NaverRealTimeResponseArea(
+        val datas: List<NaverRealTimeData>,
+        val name: String
+)
+
+@JsonClass(generateAdapter = true)
+data class NaverRealTimeData(
+        @Json(name="cd") val code: String,
+        @Json(name="nm") val name: String,
+        @Json(name="ov") val open: Int,
+        @Json(name="hv") val high: Int,
+        @Json(name="lv") val low: Int,
+        @Json(name="nv") val current: Int
+)
+
+val DUMMY_DATA = NaverRealTimeData("", "",0, 0, 0, 0)
--- app/src/main/java/com/seirion/worlddodook/data/PriceInfo.kt
+++ app/src/main/java/com/seirion/worlddodook/data/PriceInfo.kt
@@ -2,8 +2,7 @@ package com.seirion.worlddodook.data
 
 data class PriceInfo(val code: String,
                      val name: String,
-                     val current: Int,
                      val low: Int,
                      val high: Int,
                      val begin: Int,
-                     val end: Int)
+                     val current: Int)
