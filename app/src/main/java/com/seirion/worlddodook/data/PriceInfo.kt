73414de2e04cfa7fc58eca0469ac8def24db42b7
Jeong-Min Lee <falsetru@gmail.com>
2018-05-27 22:20:13 +0900

    PriceInfo as a typealias of NaverRealTimeData

--- app/src/main/java/com/seirion/worlddodook/data/PriceInfo.kt
+++ app/src/main/java/com/seirion/worlddodook/data/PriceInfo.kt
@@ -1,8 +1,3 @@
 package com.seirion.worlddodook.data
 
-data class PriceInfo(val code: String,
-                     val name: String,
-                     val low: Int,
-                     val high: Int,
-                     val begin: Int,
-                     val current: Int)
+typealias PriceInfo = NaverRealTimeData
