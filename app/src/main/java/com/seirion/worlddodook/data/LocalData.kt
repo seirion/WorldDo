29821175fe6fd89b144dc6f46a6d623936f3b1f9
hg.jang <hg.jang@sk.com>
2018-07-06 19:00:37 +0900

    Settings 클래스 추가
    - 설정 값에 필요한 필드를 모은 클래스
    - 일단 `종목 수`추가

--- app/src/main/java/com/seirion/worlddodook/data/LocalData.kt
+++ app/src/main/java/com/seirion/worlddodook/data/LocalData.kt
@@ -3,8 +3,8 @@ package com.seirion.worlddodook.data
 import android.content.Context
 import android.util.Log
 
+const val DEFAULT_PREFS = "DEFAULT_PREFS"
 private const val TAG = "LocalData"
-private const val DEFAULT_PREFS = "DEFAULT_PREFS"
 private const val PREFS_DEFAULT_KEY_CODE = "PREFS_DEFAULT_KEY_CODE"
 private const val DEFAULT_KEY_CODE = "043710" // default ㅅㅇㄹㄱ
 private const val CODE_DELIMITER = "-"
