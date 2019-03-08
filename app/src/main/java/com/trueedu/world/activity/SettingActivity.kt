ccff15991d368c5e51156890f5171adc1c10a9f3
babo <seirion@mensakorea.org>
2019-03-08 16:15:01 +0900

    functional and add life cycle

--- app/src/main/java/com/trueedu/world/activity/SettingActivity.kt
+++ app/src/main/java/com/trueedu/world/activity/SettingActivity.kt
@@ -4,18 +4,17 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
 import com.jakewharton.rxbinding3.view.focusChanges
 import com.jakewharton.rxbinding3.widget.textChangeEvents
 import com.trueedu.world.R
 import com.trueedu.world.data.Settings
+import com.trueedu.world.rx.ActivityLifecycle
+import com.trueedu.world.rx.RxAppCompatActivity
 
 
-class SettingActivity : AppCompatActivity() {
+class SettingActivity : RxAppCompatActivity() {
 
     private lateinit var codeNum: EditText
     private lateinit var coolTime: EditText
@@ -27,37 +26,29 @@ class SettingActivity : AppCompatActivity() {
 
         codeNum = findViewById(R.id.code_num)
         codeNum.setText(Settings.codeNum.toString())
-        codeNum.setOnFocusChangeListener { _, hasFocus ->
-            if (!hasFocus) {
-                Log.d(TAG, "lose focus")
-                codeNum.setText(Settings.codeNum.toString())
-            }
-        }
-        codeNum.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable) {
-                try {
-                    var num = s.toString().toInt()
-                    num = maxOf(num, Settings.MIN_CODE_NUM)
-                    num = minOf(num, Settings.MAX_CODE_NUM)
-                    Settings.codeNum = num
-                } catch (e: NumberFormatException) {
-                }
-            }
+        codeNum.focusChanges()
+                .takeUntil(getLifecycleSignal(ActivityLifecycle.DESTROY))
+                .filter { !it }
+                .doOnNext { Log.d(TAG, "lose focus") }
+                .subscribe { codeNum.setText(Settings.codeNum.toString()) }
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-        })
+        codeNum.textChangeEvents()
+                .takeUntil(getLifecycleSignal(ActivityLifecycle.DESTROY))
+                .map { it.toString().toInt() }
+                .map { maxOf(it, Settings.MIN_CODE_NUM) }
+                .map { minOf(it, Settings.MAX_CODE_NUM) }
+                .subscribe { Settings.codeNum = it }
 
         coolTime = findViewById(R.id.cool_time)
         coolTime.setText(Settings.coolTimeSec.toString())
-        coolTime.focusChanges().filter { !it }
+        coolTime.focusChanges()
+                .takeUntil(getLifecycleSignal(ActivityLifecycle.DESTROY))
+                .filter { !it }
                 .doOnNext { Log.d(TAG, "lose focus") }
                 .subscribe { coolTime.setText(Settings.coolTimeSec.toString()) }
 
         coolTime.textChangeEvents()
+                .takeUntil(getLifecycleSignal(ActivityLifecycle.DESTROY))
                 .map { it.text.toString().toLong() }
                 .map { maxOf(it, Settings.MIN_COOL_TIME_SEC) }
                 .map { minOf(it, Settings.MAX_COOL_TIME_SEC) }
